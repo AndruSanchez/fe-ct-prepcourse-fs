473bca8b36d94e8d8312997d9c53d2cb27d71bdb
AndruSanchez <andrue.sanchezl.98@gmail.com>
2023-06-07 01:38:01 -0400

    resolucion

--- M09 Ejercicios Extra/Ejercicios Extra.js
+++ M09 Ejercicios Extra/Ejercicios Extra.js
@@ -6,6 +6,12 @@ function deObjetoAarray(objeto) {
    // Estos elementos debe ser cada par clave:valor del objeto recibido.
    // [EJEMPLO]: {D: 1, B: 2, C: 3} ---> [['D', 1], ['B', 2], ['C', 3]].
    // Tu código:
+   var arr = [];
+
+   for( var key in objeto) {
+      arr.push([key, objeto[key]]);     // key = la clase   objeto[key] = el valor en [] para que sean un push de un arreglo dentro de un arreglo
+   }
+   return arr;
 }
 
 function numberOfCharacters(string) {
@@ -14,6 +20,19 @@ function numberOfCharacters(string) {
    // Las letras deben estar en orden alfabético.
    // [EJEMPLO]: "adsjfdsfsfjsdjfhacabcsbajda" ---> { a: 5, b: 2, c: 2, d: 4, f: 4, h:1, j: 4, s: 5 }
    // Tu código:
+
+   var obj = {};
+
+   for (var i = 0; i < string.length; i++) {
+      var letra = string[i]; 
+      
+      if(obj[letra]) {    // si existe suma 1, si no existe sera igual a 1. asi posteriormente pasara a 2 y seguira sumando
+      obj[letra] = obj[letra] + 1;
+      } else{
+         obj[letra] = 1;
+      }
+}
+return obj;
 }
 
 function capToFront(string) {
@@ -22,6 +41,17 @@ function capToFront(string) {
    // Retornar el string.
    // [EJEMPLO]: soyHENRY ---> HENRYsoy
    // Tu código:
+   var mayus = "";
+   var minus = "";
+
+   for(var i = 0; i < string.length; i++) {
+      if(string[i] === string[i].toUpperCase()) {
+         mayus = mayus + string[i];
+      } else {
+         minus = minus + string[i];
+      }
+   }
+   return (mayus + minus);
 }
 
 function asAmirror(frase) {
@@ -29,6 +59,8 @@ function asAmirror(frase) {
    // La diferencia es que cada palabra estará escrita al inverso.
    // [EJEMPLO]: "The Henry Challenge is close!"  ---> "ehT yrneH egnellahC si !esolc"
    // Tu código:
+
+   
 }
 
 function capicua(numero) {
